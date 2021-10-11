615cbc85361f55de2c6c590d700a0130d99915fc
MolnAtt <molnar.attila@szlgbp.hu>
2021-10-11 11:07:58 +0200

    Add project files.

---
+++ 10F_Schőn_Verem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10F_Schőn_Verem
+{
+    class Program
+    {
+        class Verem<T>
+        {
+            private class Elem<T>
+            {
+                public T ertek;
+                public Elem<T> le;
+
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
